91efe365f2a0be0217089305f57afcd0fbfe5531
bilxdi <biladi.amna@mhs.unsoed.ac.id>
2026-03-29 19:29:53 +0700

    fuzzy don ig

--- here.py
+++ here.py
@@ -8,20 +8,21 @@ kelembapan = ctrl.Antecedent(np.arange(0,100), 'kelembapan')
 kecepatan = ctrl.Consequent(np.arange(0,100), 'kecepatan')
 
 # range suhu
-suhu['Dingin'] = fuzz.trimf(suhu.universe, [0, 0, 20])
+suhu['Dingin'] = fuzz.trapmf(suhu.universe, [0, 0, 5, 15])
 suhu['Sedang'] = fuzz.trimf(suhu.universe, [10, 20, 30])
-suhu['Panas'] = fuzz.trimf(suhu.universe, [20, 40, 40])
+suhu['Panas'] = fuzz.trapmf(suhu.universe, [25, 35, 40, 40])
 
 # tingkat kelembapan
-kelembapan['Kering'] = fuzz.trimf(kelembapan.universe, [0, 2, 4])
-kelembapan['Normal'] = fuzz.trimf(kelembapan.universe, [3, 5, 7])
-kelembapan['Lembap'] = fuzz.trimf(kelembapan.universe, [6, 8, 10])
+kelembapan['Kering'] = fuzz.trapmf(kelembapan.universe, [0, 0, 20, 40])
+kelembapan['Normal'] = fuzz.trimf(kelembapan.universe, [30, 50, 70])
+kelembapan['Lembap'] = fuzz.trapmf(kelembapan.universe, [60, 80, 100, 100])
 
 # kecepatan kipas
-kecepatan['Buruk'] = fuzz.trimf(kecepatan.universe, [0, 2, 4])
-kecepatan['Cukup'] = fuzz.trimf(kecepatan.universe, [3, 5, 7])
-kecepatan['Baik'] = fuzz.trimf(kecepatan.universe, [6, 8, 10])
+kecepatan['Lambat'] = fuzz.trapmf(kecepatan.universe, [0, 0, 20, 40])
+kecepatan['Normal'] = fuzz.trimf(kecepatan.universe, [30, 50, 70])
+kecepatan['Cepat'] = fuzz.trapmf(kecepatan.universe, [60, 80, 100, 100])
 
+# view
 suhu.view()
 kelembapan.view()
 kecepatan.view()
